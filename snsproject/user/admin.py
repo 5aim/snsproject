06b803faa371c297d9c0a55ebb6e7863ff709ff4
김혁진 <hyukjin720@gmail.com>
2022-10-25 12:21:34 +0900

    create user model & tweet model and register admin page

--- snsproject/user/admin.py
+++ snsproject/user/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin # admin tool을 사용할거고
+from .models import UserModel # models.py를 불러올거고 그 중에서 UserModel을 가져올 것
 
 # Register your models here.
+admin.site.register(UserModel) # 이 코드가 나의 UserModel을 Admin에 추가 해 줍니다
