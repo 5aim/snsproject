209a47bc0c64c80959e97dd1f81262b7ee21b2d9
김혁진 <hyukjin720@gmail.com>
2022-10-26 14:28:58 +0900

    add database to tweet write function

--- snsproject/tweet/views.py
+++ snsproject/tweet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import TweetModel
 
 # 사용자가 로그인 했는지 확인해서 페이지를 나눠보여주도록 함
 def home(request):
@@ -20,3 +21,13 @@ def tweet(request):
         else:
             return redirect('/sign-in')
 
+    elif request.method == 'POST':
+        user = request.user
+
+        my_tweet = TweetModel()
+        my_tweet.author = user
+        # home.htm에 id = my-content. POST형식으로 요청한 데이터를 id, name으로 가져올 수 있음
+        my_tweet.content = request.POST.get('my-content', '')
+
+        my_tweet.save()
+        return redirect('/tweet')
