7a58def77fcd627b60781fde14f827def9c98ab2
김혁진 <hyukjin720@gmail.com>
2022-10-26 12:31:35 +0900

    tweet add urls and coding views

--- snsproject/tweet/views.py
+++ snsproject/tweet/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# 사용자가 로그인 했는지 확인해서 페이지를 나눠보여주도록 함
+def home(request):
+    user = request.user.is_authenticated
+
+    if user:
+        return redirect('/tweet')
+    else:
+        return redirect('/sign-in')
+
+
+# tweet에 home띄워주기
+def tweet(request):
+    if request.method == 'GET':
+        return render(request, 'tweet/home.html')
+
